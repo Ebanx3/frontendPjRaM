aabf927e563233938e8f71c2b2767b0b346f65fa
dos_santos_esteban <esteban2santos@gmail.com>
2022-11-04 07:54:08 -0300

    terminado frontend rick and morty project

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -5,8 +5,8 @@
       <h3 class="characterDetails">Gender: {{ char.gender }}</h3>
       <h3 class="characterDetails">Specie: {{ char.species }}</h3>
       <h3 class="characterDetails">Status: {{ char.status }}</h3>
-      <h3 class="characterDetails">Origin: {{ char.origin.name }}</h3>
-      <h3 class="characterDetails">Location: {{ char.location.name }}</h3>
+      <h3 class="characterDetails">Origin: {{ char?.origin?.name }}</h3>
+      <h3 class="characterDetails">Location: {{ char?.location?.name }}</h3>
     </div>
     <img class="characterImg" :src="char.image" :alt="char.name" />
     <div class="characterEpisodes">
@@ -19,33 +19,94 @@
         </li>
       </ul>
     </div>
-    <div class="messagesContainer"></div>
+
+    <div class="messages">
+      <form class="addMessageForm">
+        <input
+          type="text"
+          name=""
+          id="authorMsg"
+          placeholder="author"
+          v-model="author"
+        />
+        <textarea
+          name=""
+          id="contentMsg"
+          rows="10"
+          placeholder="new comment"
+          v-model="message"
+        ></textarea>
+        <input
+          type="submit"
+          value="Comment"
+          id="submitMsg"
+          @click="sendMessage"
+        />
+      </form>
+      <div class="messagesContainer">
+        <div class="message" v-for="msg in comments">
+          <p class="author">{{ msg.author }} :</p>
+          <p class="comment">{{ msg.comment }}</p>
+        </div>
+      </div>
+    </div>
   </div>
   <router-link to="/Characters" class="backButton">BACK</router-link>
 </template>
 
-<script>
-import { ref } from "vue";
+<script setup>
+import { onMounted, onUpdated, ref } from "vue";
+import { useRoute } from "vue-router";
 
 const char = ref({});
+const comments = ref([]);
+const route = useRoute();
 
-export default {
-  name: "Character",
-  data() {
-    return {
-      char,
-    };
-  },
-  created() {
-    const id = this.$router.currentRoute.value.params.id;
-    const url = `https://rickandmortyapi.com/api/character/${id}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        this.char = res;
-      });
-  },
+const author = ref("");
+const message = ref("");
+
+const fetchUrl = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      char.value = res;
+    });
 };
+
+const urlMessages = `http://localhost:8080/character/${route.params.id}/comments`;
+
+const getMessages = () => {
+  fetch(urlMessages)
+    .then((res) => res.json())
+    .then((res) => (comments.value = res.data));
+};
+
+const sendMessage = () => {
+  console.log(char.value);
+  fetch(urlMessages, {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      charId: char.value.id,
+      charName: char.value.name,
+      author: author.value,
+      comment: message.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+};
+
+onMounted(() => {
+  const id = route.params.id;
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  fetchUrl(url);
+  getMessages();
+});
+
+onUpdated(() => {
+  getMessages();
+});
 </script>
 
 <style>
@@ -65,8 +126,10 @@ export default {
 }
 
 .characterImg {
-  height: auto;
+  height: 42vh;
+  width: auto;
   border-radius: 5px;
+  margin: 0 auto;
 }
 
 .characterInfo {
@@ -137,9 +200,74 @@ export default {
   background-color: rgba(255, 255, 255, 0.788);
 }
 
-.messagesContainer {
-  border: 1px solid gray;
+.messages {
   grid-area: m;
+  display: flex;
+}
+
+.addMessageForm {
+  width: 25%;
+  padding: 10px;
+}
+
+#authorMsg {
+  border: none;
+  padding: 5px;
+  border-radius: 5px;
+  font-size: 1.1rem;
+  text-align: center;
+  font-family: "Courier New", Courier, monospace;
+  margin: 5px auto;
+  width: 90%;
+}
+
+#contentMsg {
+  font-family: "Courier New", Courier, monospace;
+  border-radius: 5px;
+  padding: 3px;
+  text-align: center;
+  resize: none;
+  width: 90%;
+}
+
+#submitMsg {
+  width: 90%;
+  padding: 4px;
+  border-radius: 5px;
+  border: none;
+  overflow: hidden;
+  background-color: rgb(91, 26, 153);
+  color: white;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-family: "Courier New", Courier, monospace;
+}
+
+#submitMsg:hover {
+  background-color: rgb(154, 70, 233);
+  cursor: pointer;
+}
+
+.messagesContainer {
+  padding: 5px;
+  overflow: hidden;
+  height: 40vh;
+  overflow-y: auto;
+}
+
+.message {
+  border: 1px solid gray;
+  margin: 2px;
+  padding: 3px;
+}
+
+.author {
+  font-size: 1.1rem;
+}
+
+.comment {
+  text-align: center;
 }
 
 @media screen and (max-width: 1024px) {
@@ -166,6 +294,7 @@ export default {
 
   .characterImg {
     width: 200px;
+    height: auto;
   }
 
   .characterInfo {
@@ -181,6 +310,12 @@ export default {
     font-size: 1rem;
     padding: 3px;
     width: 24px;
+  }
+
+  .addMessageForm {
+    width: 34%;
+    padding: 4px;
+    margin: 8px;
   }
 }
 
@@ -206,6 +341,15 @@ export default {
 
   .characterEpisodes {
     grid-column: 1/2;
+  }
+
+  .messages {
+    flex-direction: column;
+  }
+
+  .addMessageForm {
+    width: 80%;
+    margin: 0 auto;
   }
 }
 </style>
